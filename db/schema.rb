b6f1e1e6e7933a217a2b9db85c62f2c079c5b412
Jérémie Bonal <jeremie.bonal@gmail.com>
2017-09-22 22:56:21 +0200

    Added default value (false) to admin boolean in user.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170922202940) do
+ActiveRecord::Schema.define(version: 20170922205141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,7 +66,7 @@ ActiveRecord::Schema.define(version: 20170922202940) do
     t.string "first_name"
     t.string "last_name"
     t.string "phone_number"
-    t.boolean "admin"
+    t.boolean "admin", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
