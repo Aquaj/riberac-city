b077ccb6726539105b6f4c7dd0a32b9e7dbb7f65
Jérémie Bonal <jeremie.bonal@gmail.com>
2017-09-22 23:20:25 +0200

    Added Geocoder to place.

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,4 +2,7 @@ class Place < ApplicationRecord
   belongs_to :user
 
   validates :adress, :price, :transaction_type, :surface, :description, presence: true
+
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
 end
