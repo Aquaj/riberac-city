f3e816d69226430ac7b1b9ad8eead705aa44528e
Mendy Dihars <mendy.dihars@laposte.net>
2017-09-22 22:31:12 +0200

    add places controllers

---
+++ app/controllers/my/places_controller.rb
@@ -0,0 +1,11 @@
+class My::PlacesController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
---
+++ app/controllers/places_controller.rb
@@ -0,0 +1,5 @@
+class PlacesController < ApplicationController
+
+  def show
+  end
+end
