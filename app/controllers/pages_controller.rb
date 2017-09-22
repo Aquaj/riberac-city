d941dda9d03e9c4b8ddf2478583b8cd220cb8538
Jérémie Bonal <jeremie.bonal@gmail.com>
2017-09-23 00:01:00 +0200

    First version of home action.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,14 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
 
   def home
+    @categories = Category.includes(:options)
+    @places = Place.where(id: PlaceOption.where(option_id: options.select(:id)).select(:place_id))
+  end
+
+  private
+
+  def options
+    option_ids = params.permit(:option_ids)[:option_ids].to_a
+    Option.where(id: option_ids)
   end
 end
