c37837e17dab24d382e0c06c10b58002d523156f
Jérémie Bonal <jeremie.bonal@gmail.com>
2017-09-22 23:08:49 +0200

    Added validations to models.

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,3 +1,5 @@
 class Option < ApplicationRecord
   belongs_to :category
+
+  validates :name, presence: true
 end
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,3 +1,5 @@
 class Place < ApplicationRecord
   belongs_to :user
+
+  validates :adress, :price, :transaction_type, :surface, :description, presence: true
 end
