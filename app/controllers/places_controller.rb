9065b82319a1547799495571df7c4e61d6af00a6
Jérémie Bonal <jeremie.bonal@gmail.com>
2017-09-23 20:03:45 +0200

    Added index to PlacesController. Fixed issue in selected_options.

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,6 +1,11 @@
 class PlacesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:show]
 
+  def index
+    @categories = Category.includes(:options)
+    @places = matching_places.decorate
+  end
+
   def show
     @place = find_place
   end
@@ -28,5 +33,25 @@ class PlacesController < ApplicationController
     params.require(:place_attributes)
           .permit(:address, :price, :transaction_type,
                   :surface, :ges,   :energy_class, pictures: [])
+  end
+
+  def matching_places
+    places = Place.all
+    options = selected_options
+    places = places.with_options(options)
+    places = places.where(price: price_range)
+    places.order(created_at: :desc)
+  end
+
+  def selected_options
+    option_ids = params.permit(:option_ids)[:option_ids]
+    option_ids.to_a.any? ? Option.where(id: option_ids) : Option.all
+  end
+
+  def price_range
+    min = params[:min_price] || 0
+    max = params[:max_price] || Place.maximum(:price)
+    min, max = [max, min] if max > min
+    (min..max)
   end
 end
