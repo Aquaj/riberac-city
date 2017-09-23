969ca50fea072bbf72fcc9a8bf148b9c6625f5cd
Sylvain	Peigney <sylvain@volpeo.fr>
2017-09-23 20:09:32 +0200

    message de commit marrant

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,33 +3,7 @@ class PagesController < ApplicationController
 
   def home
     @categories = Category.includes(:options)
-    @places = matching_places.decorate
-
-    @markers_data = Gmaps4rails.build_markers(@places) do |place, marker|
-      marker.lat place.latitude
-      marker.lng place.longitude
-    end
+    @places = Place.all.decorate
   end
 
-  private
-
-  def matching_places
-    places = Place.all
-    # options = selected_options
-    # places = places.with_options(options)
-    # places = places.where(price: price_range)
-    # places.order(created_at: :desc)
-  end
-
-  def selected_options
-    option_ids = params.permit(:option_ids)[:option_ids]
-    option_ids.any? ? Option.where(id: option_ids) : Option.all
-  end
-
-  def price_range
-    min = params[:min_price] || 0
-    max = params[:max_price] || Place.maximum(:price)
-    min, max = [max, min] if max > min
-    (min..max)
-  end
 end
